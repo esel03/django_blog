24c5bdc64688b55e581ac8919d79b52d5f7fec13
egarmin_is <egarmin_is@magnit.ru>
2025-12-19 22:16:27 +0300

    #3: add sqlite

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,12 +74,9 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "mysite",
-        "USER": "voron",
-        "HOST": "localhost",
-        "PORT": 5432,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Путь к файлу базы данных
     }
 }
 
