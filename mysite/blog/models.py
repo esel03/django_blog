00919afd1dc92a3d51b8f3d6b96b9bfbef7e9676
egarmin_is <egarmin_is@magnit.ru>
2025-12-20 19:34:53 +0300

    #8: static, view, urls

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,29 +2,36 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-class Post (models.Model):
+
+class Post(models.Model):
 
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB', 'Published'
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(User, 
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2, 
-                              choices=Status.choices,
-                              default=Status.DRAFT)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
+
+    class PublishedPostManager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+    objects = models.Manager()
+    published = PublishedPostManager()
 
     class Meta:
-        ordering = ['-publish']
-        indexes = [models.Index(fields=['-publish'])]
+        ordering = ["-publish"]
+        indexes = [models.Index(fields=["-publish"])]
 
     def __str__(self):
         return self.title
-
-
